557cd8401f22871d4db6578b0f20c8c7b04db61d
Abhishek Singh <abhishek.singh@kuonitumlare.com>
2024-03-06 19:00:56 +0530

    Parallel movie download done

---
+++ src/apis/omdb.js
@@ -0,0 +1,5 @@
+export function searchMovie(term) {
+  return `https://www.omdbapi.com/?apiKey=${
+    import.meta.env.VITE_API_KEY
+  }&s=${term}`;
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,33 @@
+import { useEffect, useState } from "react";
+import { searchMovie } from "../apis/omdb";
+
+// Components import
+import MovieCard from "../components/MovieCard/MovieCard";
+
+// CSS imports
 import "./Home.css";
+import axios from "axios";
+
 function Home() {
+  const [movieList, setMovieList] = useState([]);
+  async function downloadDefaultMovies(...args) {
+    const urls = args.map((name) => searchMovie(name));
+    const response = await axios.all(urls.map((url) => axios.get(url)));
+    const movies = response.map((movieResponse) => movieResponse.data.Search);
+    setMovieList([].concat(...movies));
+  }
+  useEffect(() => {
+    downloadDefaultMovies("harry", "avengers", "batman");
+  }, []);
   return (
     <>
       {/* Navbar */}
       {/* Movie List */}
       {/* Pagination Buttons */}
-      <div className="movie-card-wrapper"></div>
+      <div className="movie-card-wrapper">
+        {movieList.length > 0 &&
+          movieList.map((movie) => <MovieCard key={movie.imdbID} {...movie} />)}
+      </div>
     </>
   );
 }
