d006fdbdbce31e4de6f904ab7aa6c6b680b8610a
Abhishek Singh <abhishek.singh@kuonitumlare.com>
2024-04-18 12:43:07 +0530

    Added movie details page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,14 +2,23 @@ import { useRef, useState } from "react";
 import "./Navbar.css";
 import useMovieList from "../../hooks/useMovieList";
 import useDebounce from "../../hooks/useDebounce";
+import { Link, useNavigate } from "react-router-dom";
+
 function Navbar() {
   // const resultListRef = useRef(null); // Using useRef for autocomplete input Bar
   const [isAutoCompleteVisible, setIsAutoCompleteVisible] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
   const { movieList } = useMovieList(searchTerm);
+  const navigator = useNavigate();
+  function handleAutoCompleteClick(e, movieImdbId) {
+    console.log("Id sent from menu", movieImdbId);
+    navigator(`/movie/${movieImdbId}`);
+  }
   return (
     <div className="navbar-wrapper">
-      <div>Movie Base</div>
+      <div className="movie-base-title">
+        <Link to="/">Movie Base</Link>
+      </div>
       <div className="search-bar">
         <input
           id="movie-search-input"
@@ -39,7 +48,11 @@ function Navbar() {
           <div className="autocomplete-result">Result for {searchTerm}</div>
           {movieList.length > 0 &&
             movieList.map((movie) => (
-              <div key={movie.imdbID} className="autocomplete-result">
+              <div
+                onMouseDown={(e) => handleAutoCompleteClick(e, movie.imdbID)}
+                key={movie.imdbID}
+                className="autocomplete-result"
+              >
                 {movie.Title}
               </div>
             ))}
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,5 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { searchMovieById } from "../apis/omdb";
+import MovieCard from "../components/MovieCard/MovieCard";
+
 function MovieDetails() {
-  return <>MovieDetails</>;
+  const [movie, setMovie] = useState(null);
+  const { id } = useParams();
+  async function downloadMovie() {
+    const response = await axios.get(searchMovieById(id));
+    setMovie(response.data);
+  }
+  useEffect(() => {
+    downloadMovie();
+  }, [id]);
+  return (
+    <>
+      {movie && (
+        <MovieCard
+          Title={movie.Title}
+          Year={movie.Year}
+          Type={movie.Type}
+          Poster={movie.Poster}
+        />
+      )}
+    </>
+  );
 }
 
 export default MovieDetails;
